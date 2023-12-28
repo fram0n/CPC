6db702d8c5d59425dbe80b320d4f7d08c1d15be7
fram0n <montagnoli.annafrancesca@gmail.com>
2023-12-28 12:10:10 +0100

    handson3 update2

--- HandsonAssignments/handson3/src/lib.rs
+++ HandsonAssignments/handson3/src/lib.rs
@@ -1,5 +1,11 @@
 use std::cmp;
 
+/// by using dynamic programming we build a matrix (n+1)*(d+1)
+/// so row i refers to the city i and column j refers to the days we have of vacation
+/// for cell (i,j) we can:
+/// not select city i --> matrix[i-1][j]
+/// select city i --> for day in 1..d+1 we check every (j-day) column in the previous row and add the value corrisponding to the city i with weight=day
+/// take the maximum between all these values
 pub fn holiday_planning(n:usize, d: usize, values: Vec<i32>) -> i32 {
     // create a matrix with n+1 rows and d+1 columns
     let mut matrix: Vec<Vec<i32>> = vec!(vec!(-1; d+1); n+1);
@@ -23,8 +29,12 @@ pub fn holiday_planning(n:usize, d: usize, values: Vec<i32>) -> i32 {
     return matrix[n][d];
 }
 
+/* --------------------------------------------------------- PROBLEM 2 --------------------------------------------------------- */
+
+/// first sort the topics in the vector by increasing beauty
+/// then apply Longest Increasing Subsequence on the difficulty of the topics
 pub fn design_course(n:usize, mut values: Vec<(i32, i32)>) -> usize {
-    //sort the vectors by increasing beauty
+    //sort the vector by increasing beauty
     values.sort_by(|a, b| a.0.cmp(&b.0));
     //LIS by checking the difficulty
     let mut res: Vec<(i32, i32)> = Vec::new();
@@ -43,6 +53,7 @@ pub fn design_course(n:usize, mut values: Vec<(i32, i32)>) -> usize {
     return res.len();
 }
 
+/// binary search to find the smallest element that is greater than or equal to value
 pub fn lower_bound(res: &Vec<(i32,i32)>, mut high:usize, value:i32) -> usize {
     let mut low = 0;
     while low < high {
